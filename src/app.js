ae08d6608f0a7c204d5c7d16cff15dd12e7a0d7d
姚晟 <yao_luolong@163.com>
2020-04-13 01:01:30 +0800

    登录API初步实现+response封装+https服务搭建

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const router=require('./router')
+const express = require('express')       //引入express框架
+const router=require('./router')         //引入路由
+const fs = require('fs')                 //引入用于文件操作的组件
+const https = require('https')           //引入用于搭建https服务的组件
+const bodyParser = require('body-parser')//引入用于解析request请求包的中间件
+const cors=require('cors')               //引入用于解决跨域问题的中间件
 
 const app=express()
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use('/',router)
 
-const server=app.listen(5000,function(){
-  const{address,port}=server.address()
-  console.log('http服务器已启动,http://127.0.0.1:5000/')
+const privateKey = fs.readFileSync('https/3750007_yaoluolong.xyz.key', 'utf8')
+const certificate = fs.readFileSync('https/3750007_yaoluolong.xyz.pem', 'utf8')
+const credentials = { key: privateKey, cert: certificate }
+const httpsServer = https.createServer(credentials, app)
+const SSLPORT = 18082
+
+httpsServer.listen(SSLPORT, function() {
+  console.log('HTTPS服务器已启动: https://localhost:%s', SSLPORT)
 })
--- src/router/user.js
+++ src/router/user.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const Result=require('../models/Result')
 
 const router = express.Router()
+
+router.post('/login', function(req, res, next) {
+  console.log('/user/login', req.body)
+  new Result('登录成功').success(res)
+})
 
 router.get('/info', function(req, res, next) {
   res.json('user info...')
