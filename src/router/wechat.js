498484af6232af2b816d52236caef15f8d9ae579
姚晟 <yao_luolong@163.com>
2020-05-08 14:22:51 +0800

    主页功能完成

--- src/router/wechat.js
+++ src/router/wechat.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const Result = require('../models/Result')
-const { login, create, update } = require('../service/wechat')
+const { login, create, update, recommend,hot,news } = require('../service/wechat')
 const { PRIVATE_KEY } = require('../utils/constant')
 const jwt = require('jsonwebtoken')
 const { decode } = require('../utils/decode')
@@ -61,6 +61,29 @@ router.post('/query', (req, res) => {
     } else {
       new Result('查询失败').fail(res)
     }
+  }).catch(err => {
+    console.log(err)
+  })
+})
+
+router.get('/index', (req, res) => {
+  recommend().then(result => {
+    hot().then(result2 => {
+      news().then(result3 => {
+        const recommendItems=result
+        const hotItems=result2
+        const newItems=result3
+        if (recommendItems && hotItems && newItems) {
+        new Result({ recommendItems , hotItems , newItems}, '查询成功').success(res)
+        } else {
+          new Result('查询失败').fail(res)
+        }
+      }).catch(err => {
+        console.log(err)
+      })
+    }).catch(err => {
+      console.log(err)
+    })
   }).catch(err => {
     console.log(err)
   })
--- src/service/wechat.js
+++ src/service/wechat.js
@@ -21,6 +21,51 @@ function update(openid, address, name, contact) {
   return querySql(sql)
 }
 
+function recommend() {
+  const sql = `select distinct  commodity.*
+  from evaluate
+  LEFT JOIN
+  \`order\`
+  ON
+  \`order\`.orderID=evaluate.orderID
+  LEFT JOIN
+  commodity
+  ON
+  commodity.commodityID=\`order\`.commodityID
+  WHERE
+  evaluate.score>=4`
+  return querySql(sql)
+}
+
+function hot() {
+  const sql = `select commodity.*
+  from
+  \`order\`
+  LEFT JOIN
+  commodity
+  ON
+  commodity.commodityID=\`order\`.commodityID
+  GROUP BY
+  \`order\`.commodityID`
+  return querySql(sql)
+}
+
+function news() {
+  const sql = `SELECT 
+  commodity.*,COUNT(\`order\`.commodityID)
+  FROM
+  commodity
+  LEFT JOIN
+  \`order\`
+  ON
+  \`order\`.commodityID=commodity.commodityID
+  GROUP BY
+  commodity.commodityID
+  HAVING
+  COUNT(\`order\`.commodityID)<4`
+  return querySql(sql)
+}
+
 module.exports = {
-  login, create, update
+  login, create, update, recommend, hot, news
 }
